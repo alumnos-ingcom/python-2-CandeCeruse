e5d24e34d728ce24a68da77c3b85268cacb93537
Cande <cerusecandelaria@gmail.com>
2022-06-10 02:41:34 -0300

    Subo ej 1

---
+++ src/practica/ejercicio1.py
@@ -0,0 +1,37 @@
+################
+# Candelaria Ceruse - @CandeCeruse
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+"""
+1. Pares e impares
+
+Escribir una función que retorne True cuando un número entero es par
+y False cuando no lo sea, sin utilizar módulo. (%)
+"""
+def es_entero(numero):
+    """
+    Esta funcion comprueba si el numero es entero.
+    Devuleve true si lo es y False si no lo es.
+    """
+    cociente = numero / 2
+    cociente_entero = numero // 2
+    resultado = cociente - cociente_entero
+    if resultado == 0:
+        respuesta = True
+    else:
+        respuesta = False
+    return respuesta
+
+
+def principal():
+    """
+    Esta función es la que se encarga de la parte 'interactiva' del ejercicio
+    (La entrada, la llamada al algoritmo y la salida)
+    """
+    numero = int(input("Ingrese numero entero: "))
+    respuesta = es_entero(numero)
+    print(respuesta)
+
+if __name__ == "__main__":
+    principal()
