15df315241a411e9bd32d301168215c13ca43e46
Cande <cerusecandelaria@gmail.com>
2022-06-19 17:23:27 -0300

    Subo test ej 5

---
+++ tests/test_ejercicio5.py
@@ -0,0 +1,31 @@
+################
+# Candelaria Ceruse - @CandeCeruse
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+from src.ejercicio5 import *
+
+"""
+Estos test son una función que determine si una cadena con corchetes está balanceada.
+
+Es decir, si cada corchete que abre, tiene su par que cierra. El resultado
+debe ser un valor lógico indicando si esta o no balanceado.
+"""
+
+def test_balanceado():
+    """
+    Una breve descripción del caso de prueba aplicado a la función
+    """
+    cadena = '[[]]'
+    resultado = determina_balance(cadena)
+    assert isinstance(resultado, bool)
+    assert resultado == True, "Debe ser True si está balanceado"
+
+def test_desbalanceado():
+    """
+    Una breve descripción del caso de prueba aplicado a la función
+    """
+    cadena = '[]]]'
+    resultado = determina_balance(cadena)
+    assert isinstance(resultado, bool)
+    assert resultado == False, "Debe ser False si está desbalanceado"
